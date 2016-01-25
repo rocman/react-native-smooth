3a9fab4775e4869137d323adbbde37daac6145be
rocman <rocman@vip.qq.com>
2016-01-25 14:21:24 +0800

    eliminate the vibration on the first move

--- smooth.js
+++ smooth.js
@@ -178,7 +178,7 @@ export default {
                 this.setState(meta.propsIntegrator(value), tween);
               });
             };
-            tween();
+            this.setState(meta.propsIntegrator(origin), tween);
           }
         });
       }
